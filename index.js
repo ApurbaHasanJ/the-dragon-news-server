70cd7f6103044f1c0b68875b09706bca354c043d
unknown <apurbahasanj@gmail.com>
2023-04-29 22:22:47 +0600

    varcel config file added

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
-app.get('/', (req, res) => {
-    res.send('Dragon is running')
-})
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+app.get("/", (req, res) => {
+  res.send("Dragon is running");
+});
 
-app.use(cors())
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
+app.use(cors());
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
 
-app.listen(port, ()=> {
-    console.log(`Dragon API is running on ${port}`);
-})
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoriesNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoriesNews);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Dragon API is running on ${port}`);
+});
